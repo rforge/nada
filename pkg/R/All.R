545d3423908a38d110218ec0a7b01e18829a6608
rclee <rclee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-07-19 22:44:16 +0000

    MAJOR BRANCH CHANGE!! Removed the src/ktau.f and replaced with a R-native ktau.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nada@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/All.R
+++ pkg/R/All.R
@@ -28,7 +28,7 @@
 .onLoad = function(lib, pkg) 
 {
     require(methods)
-    library.dynam("NADA", pkg, lib)
+    #library.dynam("NADA", pkg, lib)
 }
 
 NADAprobs = c(0.05,0.10,0.25,0.50,0.75,0.90,0.95)
--- pkg/R/ken.R
+++ pkg/R/ken.R
@@ -1,252 +1,279 @@
-#-->> BEGIN kendallATS aka cenken code
+#-->> BEGIN cenken code
 
 ## Generics
 
-setGeneric("cenken", 
-           function(x, x.cen, y, y.cen) standardGeneric("cenken"))
-
-## Classes 
-
+setGeneric("cenken", function(x, xcen, y, ycen) standardGeneric("cenken"))
 
 ## Private supporting routines
 
 cenken0 =
-function(x, x.cen, y, y.cen)
+function(x, xcen, y, ycen)
 {
-    kendallATS(x, x.cen, y, y.cen)
+    kendallATS(x, xcen, y, ycen)
 }
 
 cenken1 =
-function(x, x.cen, y, y.cen)
+function(x, xcen, y, ycen)
 {
     xc = rep(FALSE, length(x))
-    kendallATS(x, xc, x.cen, y)
+    kendallATS(x, xc, xcen, y)
 }
 
 # routine for cenken("numeric", "Cen")
 cenken.unicen =
-function(x, x.cen, y, y.cen)
+function(x, xcen, y, ycen)
 {
-    cenken1 (x, x.cen@Surv[,1], !x.cen@Surv[,2])
+    cenken1 (x, xcen@Surv[,1], !xcen@Surv[,2])
 }
 
 # routine for cenken("Cen", "Cen")
 cenken.bicen =
-function(x, x.cen, y, y.cen)
+function(x, xcen, y, ycen)
 {
-    kendallATS(x@Surv[,1], !x@Surv[,2], x.cen@Surv[,1], !x.cen@Surv[,2])
+    kendallATS(x@Surv[,1], !x@Surv[,2], xcen@Surv[,1], !xcen@Surv[,2])
 }
 
 ## Methods
 
 setMethod("cenken",
-          signature(x="numeric", x.cen="logical", y="numeric", y.cen="logical"),
+          signature(x="numeric", xcen="logical", y="numeric", ycen="logical"),
           cenken0)
 
 setMethod("cenken",
-          signature(x="numeric", x.cen="missing", y="numeric", y.cen="logical"),
+          signature(x="numeric", xcen="missing", y="numeric", ycen="logical"),
           cenken1)
 
 setMethod("cenken",
-          signature(x="numeric", x.cen="numeric", y="logical", y.cen="missing"),
+          signature(x="numeric", xcen="numeric", y="logical", ycen="missing"),
           cenken1)
 
 setMethod("cenken",
-          signature(x="numeric", x.cen="Cen", y="missing", y.cen="missing"),
+          signature(x="numeric", xcen="Cen", y="missing", ycen="missing"),
           cenken.unicen)
 
 setMethod("cenken",
-          signature(x="Cen", x.cen="Cen", y="missing", y.cen="missing"),
+          signature(x="Cen", xcen="Cen", y="missing", ycen="missing"),
           cenken.bicen)
 
 ## kendallATS function -- the heart of the cenken routines
-#  Original code written by D Lorenz (USGS) for S-Plus
+#  Original S code written by D Lorenz (USGS) for S-Plus.
+#  Port to R and R-native ktau function by L Lee and D Helsel.
 #
-#  Port to R by Lopaka Lee,  June 2006
-#  Requires ktau.f built as a native shared object
-
 # Kendall's tau used as an estimate of the relation between x and y
 #    with the ATS slope estimator (x and y left-censored).
 #
-# Coding History:
-#    2005Jun30 DLLorenz original code modifed from Dennis Helsel Minitab macro
-#    2005Jul14 DLLorenz date fix
-#    2006Apr11 DLLorenz Modifed to print slope and medians
-#    2006Jun05 DLLorenz Modifed bounds on slope estimates
-#    2006Jun05     This version.
-#
-
-kendallATS = 
-function(x, x.cen, y, y.cen) 
+kendallATS =
+function(x, xcen, y, ycen, tol=1e-7, iter=1e+3) 
 {
-  if(any(is.na(x) | is.na(y))) stop("Missing values not allowed")
+    # Jitter y -- this is only needed in ktau_s but is done here
+    # because the original Fortran code (the "standard") did.
+    y = y - (min(y)/1000) * ycen 
 
-  n <- length(x)
-  if(length(x) < 3) stop("y and x should effectively be longer than 2")
+    iter_s = 
+    function(lb, ub, x, xcen, y, ycen, tol=tol, iter=iter) 
+    {
+        step_s = 
+        function(lb, ub, x, xcen, y, ycen)
+        {
+          b = c(lb, (lb+ub)/2, ub)
+          res = sapply(b, function(i) y - i * x)
+          s = apply(res, 2, ktau_s, x=x, xcen=xcen, ycen=ycen)
 
-  ## Median of the y values.
-  median.y <- median(cenros(y, y.cen))
-  if(is.na(median.y)) median.y <- min(y)
+          list(b=b, s=s)
+        }
 
-  ## Median of the x values.
-  median.x <- median(cenros(x, x.cen))
-  if(is.na(median.x)) median.x <- min(x)
+        bs = step_s(lb, ub, x, xcen, y, ycen)
 
-  ## compute s, vars, tau, tau-b
-  xx <- x
-  cx <- x.cen
-  yy <- y
-  cy <- y.cen
-  delx <- min(diff(sort(unique(xx)))) / 1000.
-  dely <- min(diff(sort(unique(yy)))) / 1000.
-  dupx <- xx - delx * cx
-  diffx <- outer(dupx, dupx, "-")
-  diffcx <- outer(cx, cx, "-")
-  xplus <- outer(cx, -cx, "-")
-  dupy <- yy - dely * cy
-  diffy <- outer(dupy, dupy, "-")
-  diffcy <- outer(cy, cy, "-")
-  yplus <- outer(cy, -cy, "-")
-  signyx <- sign(diffy * diffx)
-  tt <- (sum(1-abs(sign(diffx)))-n)/2   # no. of pairwise ties in x
-  uu <- (sum(1-abs(sign(diffy)))-n)/2   # no. of pairwise ties in y
-  cix <- sign(diffcx)*sign(diffx)
-  cix <- ifelse(cix <= 0, 0, 1)
-  tt <- tt + sum(cix) / 2
-  signyx <- signyx * (1 - cix)
-  ciy <- sign(diffcy)*sign(diffy)
-  ciy <- ifelse(ciy <= 0, 0, 1)
-  uu <- uu + sum(ciy) / 2
-  signyx <- signyx * (1 - ciy)
-  xplus <- ifelse(xplus <= 1, 0, 1)
-  yplus <- ifelse(yplus <= 1, 0, 1)
-  diffx <- abs(sign(diffx))
-  diffy <- abs(sign(diffy))
-  tplus <- xplus*diffx
-  uplus <- yplus*diffy
-  tt <- tt + sum(tplus) / 2
-  uu <- uu + sum(uplus) / 2
-  itot <- sum(signyx * (1- xplus) * (1 - yplus))
-  kenS <- itot/2 # kendall's S for original data
-  tau <- (itot)/(n*(n-1))    # Kendall's tau-a for original data
-  J <- n * (n - 1) / 2
-  taub <- kenS/(sqrt(J - tt) * sqrt(J - uu))
-  varS <- n*(n-1)*(2*n+5)/18   # var without tie correction
-  ##  adjust varS for ties
-  ##  taken from tiesboth.mac by Ed Gilroy
-  ##  and modified for censored data by Dennis Helsel.
-  ##  extracted from ckend.mac
-  ## variance adjusted for ties between censored observations
-  intg <- 1:n
-  dupx <- xx - delx * cx # offset censored values by a small amount
-  dupy <- yy - dely * cy
-  dorder <- order(dupx)
-  dxx <- dupx[dorder]
-  dcx <- cx[dorder]
-  dorder <- order(dupy)
-  dyy <- dupy[dorder]
-  dcy <- cy[dorder]
-  tmpx <- dxx - intg * (1 - dcx) * delx
-  tmpy <- dyy - intg * (1 - dcy) * dely
-  rxlng <- rle(rank(tmpx))$lengths
-  nrxlng <- table(rxlng)
-  rxlng <- as.integer(names(nrxlng))
-  x1 <- nrxlng * rxlng * (rxlng - 1) * (2 * rxlng + 5)
-  x2 <- nrxlng * rxlng * (rxlng - 1) * (rxlng - 2)
-  x3 <- nrxlng * rxlng * (rxlng - 1)
-  rylng <- rle(rank(tmpy))$lengths
-  nrylng <- table(rylng)
-  rylng <- as.integer(names(nrylng))
-  y1 <- nrylng * rylng * (rylng - 1) * (2 * rylng + 5)
-  y2 <- nrylng * rylng * (rylng - 1) * (rylng - 2)
-  y3 <- nrylng * rylng * (rylng - 1)
-  delc <- (sum(x1) + sum(y1)) / 18 -
-    sum(x2) * sum(y2) / (9 * n * (n - 1) * (n - 2)) -
-      sum(x3) * sum(y3) / ( 2 * n * (n - 1))
-  ## correction for C-C pairs double counted as UC-C pairs
-  ## made to deluc
-  x4 <- nrxlng * (rxlng - 1)
-  y4 <- nrylng * (rylng - 1)
-  ## variance adjusted for ties in censored and uncensored obs.
-  tmpx <- intg * dcx - 1
-  tmpx <- ifelse(tmpx < 0, 0, tmpx)
-  nrxlng <- sum(tmpx)
-  rxlng <- 2
-  x1 <- nrxlng * rxlng * (rxlng - 1) * (2 * rxlng + 5)
-  x2 <- nrxlng * rxlng * (rxlng - 1) * (rxlng - 2)
-  x3 <- nrxlng * rxlng * (rxlng - 1)
-  tmpy <- intg * dcy - 1
-  tmpy <- ifelse(tmpy < 0, 0, tmpy)
-  nrylng <- sum(tmpy)
-  rylng <- 2
-  y1 <- nrylng * rylng * (rylng - 1) * (2 * rylng + 5)
-  y2 <- nrylng * rylng * (rylng - 1) * (rylng - 2)
-  y3 <- nrylng * rylng * (rylng - 1)
-  deluc <- (sum(x1) + sum(y1)) / 18 -
-    sum(x2) * sum(y2) / (9 * n * (n - 1) * (n - 2)) -
-      sum(x3) * sum(y3) / ( 2 * n * (n - 1)) - (sum(x4) + sum(y4))
-  ## variance adjustment for ties between uncensored obs.
-  dxx <- dxx - intg * dcx * delx
-  dyy <- dyy - intg * dcy * dely
-  rxlng <- rle(rank(dxx))$lengths
-  nrxlng <- table(rxlng)
-  rxlng <- as.integer(names(nrxlng))
-  x1 <- nrxlng * rxlng * (rxlng - 1) * (2 * rxlng + 5)
-  x2 <- nrxlng * rxlng * (rxlng - 1) * (rxlng - 2)
-  x3 <- nrxlng * rxlng * (rxlng - 1)
-  rylng <- rle(rank(dyy))$lengths
-  nrylng <- table(rylng)
-  rylng <- as.integer(names(nrylng))
-  y1 <- nrylng * rylng * (rylng - 1) * (2 * rylng + 5)
-  y2 <- nrylng * rylng * (rylng - 1) * (rylng - 2)
-  y3 <- nrylng * rylng * (rylng - 1)
-  delu <- (sum(x1) + sum(y1)) / 18 -
-    sum(x2) * sum(y2) / (9 * n * (n - 1) * (n - 2)) -
-      sum(x3) * sum(y3) / ( 2 * n * (n - 1))
-  varS <- varS - delc - deluc - delu
-  p.val <- 2 * (1 - pnorm((abs(kenS - sign(kenS)))/sqrt(varS)))
-  ## prepare for ATS slope estimate
-  flipx <- max(xx) - xx + 1
-  cx <- 1 - cx
-  flipy <- max(yy) - yy + 1
-  cy <- 1 - cy
-  slopes <- unlist(lapply(seq(along=xx), function(i, y, x) 
-                          ((y[i] - y[1:i]) / (x[i] - x[1:i])), 
-                            yy * cy, xx * cx)) 
-                            # substitute zeros for censored values 
-  bounds <- range(slopes[is.finite(slopes)]) 
-  iter <- 1000
-  result <- .Fortran("ktau",
-                     x = as.double(flipx), y = as.double(flipy),
-                     icx = as.integer(cx), icy = as.integer(cy),
-                     num = as.integer(n), slope = double(1),
-                     lbound = as.double(bounds[1]),
-                     ubound = as.double(bounds[2]),
-                     dev = double(1), iter = as.integer(iter))
-  sen.slope <- result$slope
-  if(result$iter >= iter)
-    warning("Slope estimation did not converge")
-  ## A line representing the trend of the data then is given by
-                                        #
-  ##    y(t) = sen.slope*(t-med.time)+med.data
-                                        #
-  ##    This line has the slope of the trend and passes through
-  ##       the point (t=med.time, y=med.data)
-  ## Compute the coefficients for the line
-  coef <- c(sen.slope*(-median.x)+median.y, sen.slope) # intercept and slope
-  ## Return the statistics.
-  names(taub) <- "tau"
-  zero <- 0
-  names(zero) <- "slope"
-  est <- c(sen.slope, median.y, median.x)
-  names(est) <- c("slope", "median.y", "median.x")
-  method <- "Kendall's tau with the ATS slope estimator"
-  z <- list(method = method, statistic = taub, p.value = p.val,
-            estimate = est, alternative="two.sided",
-            coef=coef, slope.se = result$dev, taua=tau)
-  z$data.name <-  paste(deparse(substitute(x)), "and",deparse(substitute(y)))
-  oldClass(z) <- "htest"
-  return(z)
+        for (i in 1:iter)
+          {
+            b = bs$b
+            s = bs$s
+
+            if ((s[1] * s[2]) <= 0) { s[3] = s[2]; b[3] = b[2] }
+            else                    { s[1] = s[2]; b[1] = b[2] }
+
+            if ( (s[2] == 0) || (abs(b[3] - b[1]) <= tol) ) break()
+
+            bs = step_s(b[1], b[3], x, xcen, y, ycen)
+          }
+        return(bs)
+    }
+
+    k = ktau_b(x, xcen, y, ycen)
+    bs = iter_s(k[1], k[2], x, xcen, y, ycen, tol=tol, iter=iter)
+
+    b = bs$b
+    s = bs$s
+
+    if ( (s[2] != 0) || (abs(b[3] - b[1]) > tol) ) slope = b[2]
+    else
+      {
+       cat("Dropped in to refining slope ...\n")
+       ubs = iter_s(b[2], b[3], x, xcen, y, ycen, tol=tol, iter=iter)
+       lbs = iter_s(b[1], b[2], x, xcen, y, ycen, tol=tol, iter=iter)
+
+       slope = 0.5 * (ubs$b[2] + lbs$b[2])
+      }
+    
+    ## Calculate the intercept
+    #res = y - slope * x
+    ## offset residuals to make them positive
+    #offs = abs(min(res)) + 1
+    #res = res + offs
+    ## use ROS to calculate the intercept
+    #int = median(cenros(res, ycen)) - offs
+
+    p_tau = ktau_p(x, xcen, y, ycen)
+
+    return(list(tau=p_tau$tau, slope=slope, p=p_tau$p))
 }
 
-#-->> END kendallATS aka cenken code
+ktau_b =
+function (x, xcen, y, ycen) 
+{
+    cx = !xcen
+    cy = !ycen
+
+    slopes = unlist(lapply(seq(along = x), function(i, y, x) ((y[i] - 
+        y[1:i])/(x[i] - x[1:i])), y * cy, x * cx))
+
+    return(range(slopes[is.finite(slopes)]))
+}
+
+ktau_s =
+function(x, xcen, y, ycen)
+{
+    # jitter y
+    #y = y - (min(y)/1000) * ycen
+
+    ## Find signs of all x<->y slopes (xy2)
+    xy1 = sign(outer(y, y, "-"))       
+    xy2 = xy1 * sign(outer(x, x, "-"))
+
+    itot = 0.5 * sum( (outer(ycen, -ycen, "-") == 0) * xy2)
+
+    xy3 = outer(ycen, ycen, "-")
+    xy4 = outer(y, y, "<=")
+
+    itot = itot + sum(((xy3 * xy4) == 1) * xy2)
+
+    return(itot)
+}
+
+ktau_p =
+function (x, xcen, y, ycen) 
+{
+    xx <- x
+    cx <- xcen
+    yy <- y
+    cy <- ycen
+    n  <- length(xx)
+
+    delx <- min(diff(sort(unique(xx))))/1000
+    dely <- min(diff(sort(unique(yy))))/1000
+    dupx <- xx - delx * cx
+    diffx <- outer(dupx, dupx, "-")
+    diffcx <- outer(cx, cx, "-")
+    xplus <- outer(cx, -cx, "-")
+    dupy <- yy - dely * cy
+    diffy <- outer(dupy, dupy, "-")
+    diffcy <- outer(cy, cy, "-")
+    yplus <- outer(cy, -cy, "-")
+    signyx <- sign(diffy * diffx)
+    tt <- (sum(1 - abs(sign(diffx))) - n)/2
+    uu <- (sum(1 - abs(sign(diffy))) - n)/2
+    cix <- sign(diffcx) * sign(diffx)
+    cix <- ifelse(cix <= 0, 0, 1)
+    tt <- tt + sum(cix)/2
+    signyx <- signyx * (1 - cix)
+    ciy <- sign(diffcy) * sign(diffy)
+    ciy <- ifelse(ciy <= 0, 0, 1)
+    uu <- uu + sum(ciy)/2
+    signyx <- signyx * (1 - ciy)
+    xplus <- ifelse(xplus <= 1, 0, 1)
+    yplus <- ifelse(yplus <= 1, 0, 1)
+    diffx <- abs(sign(diffx))
+    diffy <- abs(sign(diffy))
+    tplus <- xplus * diffx
+    uplus <- yplus * diffy
+    tt <- tt + sum(tplus)/2
+    uu <- uu + sum(uplus)/2
+
+    itot <- sum(signyx * (1 - xplus) * (1 - yplus))
+    kenS <- itot/2
+    tau <- (itot)/(n * (n - 1))
+
+    J <- n * (n - 1)/2
+    taub <- kenS/(sqrt(J - tt) * sqrt(J - uu))
+    varS <- n * (n - 1) * (2 * n + 5)/18
+    intg <- 1:n
+    dupx <- xx - delx * cx
+    dupy <- yy - dely * cy
+    dorder <- order(dupx)
+    dxx <- dupx[dorder]
+    dcx <- cx[dorder]
+    dorder <- order(dupy)
+    dyy <- dupy[dorder]
+    dcy <- cy[dorder]
+    tmpx <- dxx - intg * (1 - dcx) * delx
+    tmpy <- dyy - intg * (1 - dcy) * dely
+    rxlng <- rle(rank(tmpx))$lengths
+    nrxlng <- table(rxlng)
+    rxlng <- as.integer(names(nrxlng))
+    x1 <- nrxlng * rxlng * (rxlng - 1) * (2 * rxlng + 5)
+    x2 <- nrxlng * rxlng * (rxlng - 1) * (rxlng - 2)
+    x3 <- nrxlng * rxlng * (rxlng - 1)
+    rylng <- rle(rank(tmpy))$lengths
+    nrylng <- table(rylng)
+    rylng <- as.integer(names(nrylng))
+    y1 <- nrylng * rylng * (rylng - 1) * (2 * rylng + 5)
+    y2 <- nrylng * rylng * (rylng - 1) * (rylng - 2)
+    y3 <- nrylng * rylng * (rylng - 1)
+    delc <- (sum(x1) + sum(y1))/18 - sum(x2) * sum(y2)/(9 * n * 
+        (n - 1) * (n - 2)) - sum(x3) * sum(y3)/(2 * n * (n - 
+        1))
+    x4 <- nrxlng * (rxlng - 1)
+    y4 <- nrylng * (rylng - 1)
+    tmpx <- intg * dcx - 1
+    tmpx <- ifelse(tmpx < 0, 0, tmpx)
+    nrxlng <- sum(tmpx)
+    rxlng <- 2
+    x1 <- nrxlng * rxlng * (rxlng - 1) * (2 * rxlng + 5)
+    x2 <- nrxlng * rxlng * (rxlng - 1) * (rxlng - 2)
+    x3 <- nrxlng * rxlng * (rxlng - 1)
+    tmpy <- intg * dcy - 1
+    tmpy <- ifelse(tmpy < 0, 0, tmpy)
+    nrylng <- sum(tmpy)
+    rylng <- 2
+    y1 <- nrylng * rylng * (rylng - 1) * (2 * rylng + 5)
+    y2 <- nrylng * rylng * (rylng - 1) * (rylng - 2)
+    y3 <- nrylng * rylng * (rylng - 1)
+    deluc <- (sum(x1) + sum(y1))/18 - sum(x2) * sum(y2)/(9 * 
+        n * (n - 1) * (n - 2)) - sum(x3) * sum(y3)/(2 * n * (n - 
+        1)) - (sum(x4) + sum(y4))
+    dxx <- dxx - intg * dcx * delx
+    dyy <- dyy - intg * dcy * dely
+    rxlng <- rle(rank(dxx))$lengths
+    nrxlng <- table(rxlng)
+    rxlng <- as.integer(names(nrxlng))
+    x1 <- nrxlng * rxlng * (rxlng - 1) * (2 * rxlng + 5)
+    x2 <- nrxlng * rxlng * (rxlng - 1) * (rxlng - 2)
+    x3 <- nrxlng * rxlng * (rxlng - 1)
+    rylng <- rle(rank(dyy))$lengths
+    nrylng <- table(rylng)
+    rylng <- as.integer(names(nrylng))
+    y1 <- nrylng * rylng * (rylng - 1) * (2 * rylng + 5)
+    y2 <- nrylng * rylng * (rylng - 1) * (rylng - 2)
+    y3 <- nrylng * rylng * (rylng - 1)
+    delu <- (sum(x1) + sum(y1))/18 - sum(x2) * sum(y2)/(9 * n * 
+        (n - 1) * (n - 2)) - sum(x3) * sum(y3)/(2 * n * (n - 
+        1))
+
+    varS <- varS - delc - deluc - delu
+
+    p.val <- 2 * (1 - pnorm((abs(kenS - sign(kenS)))/sqrt(varS)))
+
+    return(list(tau=tau, p=p.val))
+}
+
+
